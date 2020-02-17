76d815cf4ab9674fd99a7fb5ef0451712fda77d9
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-02-17 18:40:21 -0500

    establish connection to local mongoDB

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/a11y-scans', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+db.on('error', err => {
+  console.error('MongoDB error: ' + err.message)
+  process.exit(1)
+})
+db.once('open', () => console.log('MongoDB connection established'))
