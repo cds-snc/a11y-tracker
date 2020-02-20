922485a70e0d39eedffd5c2087d891b501c66595
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-02-20 15:57:10 -0500

    temporarily log api post/db insert success to console

--- routes/api/api.controller.js
+++ routes/api/api.controller.js
@@ -2,6 +2,10 @@ const A11yScan = require('../../models/a11y-scan.js')
 
 const addA11yScanResult = async (req, res) => {
   await new A11yScan({result: req.body}).save()
+  A11yScan.countDocuments({}, (err, nScans) => {
+    console.log("You've inserted a new result into the A11yScan DB table")
+    console.log(`There are now ${nScans} scans in the table`)
+  });
   res.json({ message: 'success' })
 }
 
