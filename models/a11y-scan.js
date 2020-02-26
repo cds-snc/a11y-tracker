9864b7b9370877251b6fb6b0d43cd17016b55210
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-02-26 12:50:17 -0500

    A11yScan model schema should include primary axe-core result fields

--- models/a11y-scan.js
+++ models/a11y-scan.js
@@ -3,10 +3,17 @@ const Schema = mongoose.Schema
 
 const a11yScanSchema = new Schema({
   url: String, // the URL that was scanned
-  project_name: String, // short description of the project/app the URL belongs to (e.g. "CPPD Disability Benefit" )
+  testEnvironment: {}, // state of the client/browser during the scan
+  timeStamp: Date,
+  violations: [],
+  passes: [],
+  incomplete: [],
+  inapplicable: [],
+  axe_meta_data: {},
+  // meta-data fields that do not mirror axe-core result object fields 
   scan_name: String, // short description of the state of the webpage/DOM while the scan was run (e.g. "Invalid login state")
+  project_name: String, // short description of the project/app the URL belongs to (e.g. "CPPD Disability Benefit" )
   revision: String, // the version of the project/app that was scanned (e.g. Git hash or version tag) 
-  result: {}, 
 })
 
 const A11yScan = mongoose.model('A11yScan', a11yScanSchema)
