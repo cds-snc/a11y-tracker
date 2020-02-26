c6219c3589d3f3980167c59c6859674a4b7e998a
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-02-26 11:50:55 -0500

    A11yScan model should have meta data fields

--- models/a11y-scan.js
+++ models/a11y-scan.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const a11yScanSchema = mongoose.Schema({
-  result: {}
+const a11yScanSchema = new Schema({
+  url: String, // the URL that was scanned
+  project_name: String, // short description of the project/app the URL belongs to (e.g. "CPPD Disability Benefit" )
+  scan_name: String, // short description of the state of the webpage/DOM while the scan was run (e.g. "Invalid login state")
+  revision: String, // the version of the project/app that was scanned (e.g. Git hash or version tag) 
+  result: {}, 
 })
 
 const A11yScan = mongoose.model('A11yScan', a11yScanSchema)
