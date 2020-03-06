affa07973c9d7ec92d493e9678811d48e4eaa98f
Max Neuvians <max@neuvians.net>
2020-03-06 13:28:46 -0500

    Wrap in try fail

--- routes/api/api.controller.js
+++ routes/api/api.controller.js
@@ -1,28 +1,32 @@
 const A11yScan = require('../../models/a11y-scan.js')
 
 const addA11yScanResult = async (req, res) => {
-  const _json = req.body // JSON data representing one scan 
+  try {
+    const _json = req.body // JSON data representing one scan 
 
-  const newA11yScanModel = new A11yScan({
-    url: _json.result.url,
-    testEnvironment: _json.result.testEnvironment,
-    timeStamp: _json.result.timestamp,
-    violations: _json.result.violations,
-    passes: _json.result.passes,
-    incomplete: _json.result.incomplete,
-    inapplicable: _json.result.inapplicable,
-    axe_meta_data: {
-      testEngine: _json.result.testEngine,
-      testRunner: _json.result.testRunner,
-      toolOptions: _json.result.toolOptions,
-    },
-    project_name: _json.project_name,
-    scan_name: _json.scan_name,
-    revision: _json.revision,
-  })
+    const newA11yScanModel = new A11yScan({
+      url: _json.result.url,
+      testEnvironment: _json.result.testEnvironment,
+      timeStamp: _json.result.timestamp,
+      violations: _json.result.violations,
+      passes: _json.result.passes,
+      incomplete: _json.result.incomplete,
+      inapplicable: _json.result.inapplicable,
+      axe_meta_data: {
+        testEngine: _json.result.testEngine,
+        testRunner: _json.result.testRunner,
+        toolOptions: _json.result.toolOptions,
+      },
+      project_name: _json.project_name,
+      scan_name: _json.scan_name,
+      revision: _json.revision,
+    })
 
-  await newA11yScanModel.save()  
-  res.json({ message: 'success' })
+    await newA11yScanModel.save()  
+    res.json({ message: 'success' })
+  } catch {
+    res.json({ message: 'Failed to save! Check schema!' })
+  }
 }
 
 module.exports = (app) => {
