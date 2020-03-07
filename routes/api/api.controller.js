be6b465b2e11177f4f36ef4b237f83ddcfed22c8
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-03-06 19:11:09 -0500

    A11yScan model should be responsible for transforming an axe result into a DB document

--- routes/api/api.controller.js
+++ routes/api/api.controller.js
@@ -3,26 +3,7 @@ const A11yScan = require('../../models/a11y-scan.js')
 const addA11yScanResult = async (req, res) => {
   try {
     const _json = req.body // JSON data representing one scan 
-
-    const newA11yScanModel = new A11yScan({
-      url: _json.result.url,
-      testEnvironment: _json.result.testEnvironment,
-      timeStamp: _json.result.timestamp,
-      violations: _json.result.violations,
-      passes: _json.result.passes,
-      incomplete: _json.result.incomplete,
-      inapplicable: _json.result.inapplicable,
-      axe_meta_data: {
-        testEngine: _json.result.testEngine,
-        testRunner: _json.result.testRunner,
-        toolOptions: _json.result.toolOptions,
-      },
-      project_name: _json.project_name,
-      scan_name: _json.scan_name,
-      revision: _json.revision,
-    })
-
-    await newA11yScanModel.save()  
+    await A11yScan.insertAxeCoreResult(_json.result, _json.project_name, _json.scan_name, _json.revision)  
     res.json({ message: 'success' })
   } catch {
     res.json({ message: 'Failed to save! Check schema!' })
