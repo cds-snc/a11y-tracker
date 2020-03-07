926bec1049499430a200fe3edffa3f5bc2f9b44c
Max Neuvians <max@neuvians.net>
2020-03-07 12:03:11 -0500

    Fix command line

--- bin/cli.js
+++ bin/cli.js
@@ -1,43 +1,24 @@
 #!/usr/bin/env node
-var _routesConfig = require('../config/routes.config.js')
-const path = require('path')
-const fs = require('fs');
+const program = require('commander');
 
-
-const commander = require('commander')
-const program = new commander.Command()
-program.version('0.0.1')
-
-program.option('a11y', 'Generates the a11y json')
-program.option('routes', 'Prints out all the routes')
-program.option('-k, --key <name>', 'Generates an API key for and organisation')
-
-program.parse(process.argv)
-
-if (program.a11y) {
-  const paths = _routesConfig.routes.map(p => p.path.replace("/", ""))
-  const object = {"urls": paths}
-  const outDir = path.join(process.cwd())
-  const outFile = "a11y.json"
-
-  fs.writeFile(`${outDir}/${outFile}`, JSON.stringify(object), function (err) {
-    if (err) throw err;
-    console.log('Wrote paths for a11y checker');
-  });
-
-}
-
-if (program.routes) {
-  console.log(_routesConfig.routes)
-}
-
-if (program.key) {
-  //require('./db')
-  const ApiKey = require('../models/api-key.js')
-  console.log("Generating API key for", program.key, "...")
-  const key = new ApiKey({
-    organisation: program.key,
+program
+  .version('0.0.1')
+  .command('key <name>')
+  .description('Generates an API key for and organisation')
+  .action((name) => {
+    require('./../db')
+    const ApiKey = require('../models/api-key.js')
+    console.log("Generating API key for", name, "...")
+    const key = new ApiKey({
+      organisation: name,
+    })
+    key.save().then(() => {
+      console.log("API Key for", name, ':', key.key)
+      return process.exit(0);
+    })
+    
   })
-  //await key.save() 
-  console.log("API Key for", program.key, ':', key.key)
-}
+
+program.parse(process.argv);
+
+
