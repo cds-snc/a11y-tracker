fed6f80ec7efeb91a3a221629ae25d0c8f6740fc
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-03-09 17:25:28 -0400

    add axe rules JSON to project

--- routes/mock-data/mock-data.controller.js
+++ routes/mock-data/mock-data.controller.js
@@ -1,5 +1,6 @@
 const A11yScan = require('../../models/a11y-scan.js')
 const vacScan = require('./data/vac-axe-results.json')
+const axeRules = require('./data/axe-rules.json')
 
 const mockProjectName = "Mock Data : Find Benefits and Services"
 
