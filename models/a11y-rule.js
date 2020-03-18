584e688fb8e0cc214a275ddcd7be8592a2b47c28
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-03-18 16:43:11 -0400

    memoize sum of rule weights and update the var when rules are saved or deleted

--- models/a11y-rule.js
+++ models/a11y-rule.js
@@ -11,11 +11,29 @@ const a11yRuleSchema = new Schema({
 }, {
   timestamps: true
 })
+ 
+a11yRuleSchema.statics.getTotalWeights = function() {
+  if (totalWeights === undefined) sumAllWeights()
+  return totalWeights
+} 
 
-a11yRuleSchema.statics.sumAllWeights = function() {
-  return this.aggregate().
+a11yRuleSchema.post('save', function(rule) {
+  sumAllWeights()
+  console.log('%s has been saved', rule.ruleId);
+});
+
+a11yRuleSchema.post('remove', function(rule) {
+  sumAllWeights()
+  console.log('%s has been removed', rule.ruleId);
+});
+
+const A11yRule = mongoose.model('A11yRule', a11yRuleSchema)
+let totalWeights
+
+const sumAllWeights = function() {
+  totalWeights = A11yRule.aggregate().
     group({ _id: null, total: { $sum: "$weight"  }}).
-    then(result => result[0].total)
-}  
+    then(result => result[0] === undefined ? 0 : result[0].total)
+} 
 
-module.exports = mongoose.model('A11yRule', a11yRuleSchema)
+module.exports = A11yRule 
